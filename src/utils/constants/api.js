cc3c45466c97b6b54feb0de3cca49b197f868180
PratikMeghnathi <140478668+PratikMeghnathi@users.noreply.github.com>
2025-06-03 15:04:44 +0530

    Last commit(4)

--- src/utils/constants/api.js
+++ src/utils/constants/api.js
@@ -9,7 +9,8 @@
  * @default 'http://localhost:8080/api'
  */
 
-export const API_BASE_URL = import.meta.env.NODE_ENV === "production" ? import.meta.env.VITE_API_BASE_URL : import.meta.env.VITE_LOCAL_API_BASE_URL
+export const API_BASE_URL = import.meta.env.VITE_ENV === "production" ? import.meta.env.VITE_API_BASE_URL : import.meta.env.VITE_LOCAL_API_BASE_URL
+console.log({ env: import.meta.env.VITE_ENV, cloud: import.meta.env.VITE_API_BASE_URL, local: import.meta.env.VITE_LOCAL_API_BASE_URL })
 
 /**
  * Collection of API endpoints organized by feature
